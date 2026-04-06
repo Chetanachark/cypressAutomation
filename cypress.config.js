6c3fa28501db4139a66a89e74a592873b24260fd
Chetanachark <155732792+Chetanachark@users.noreply.github.com>
2026-04-06 14:46:11 +0530

    Update specPattern to include all integration tests

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern:"cypress/integration/examples/*.js",
+    specPattern:"cypress/integration/**/*.js",
     retries: {
       runMode: 2,
       openMode: 0
