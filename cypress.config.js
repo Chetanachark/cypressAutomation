f614087cd9c4100a076f528fd7b06c0b951ec003
Chetanachar <chetanachar.k@gmail.com>
2026-04-07 12:40:45 +0530

    fixing yaml

--- cypress.config.js
+++ cypress.config.js
@@ -3,20 +3,21 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'iriqwm',
 
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: false,
-    json: true
-  },
-
   e2e: {
     specPattern: "cypress/integration/**/*.js",
     supportFile: false,
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    },
+
     retries: {
       runMode: 2,
       openMode: 0
     }
-  },
+  }
 });
